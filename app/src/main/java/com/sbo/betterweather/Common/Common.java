d883a5fbd1cd6b984579be2c8b27e2b2a7d64145
SbongiseniMbaraga <sbongiseni2@hotmail.com>
2020-05-16 17:10:53 +0200

    Initial commit

---
+++ app/src/main/java/com/sbo/betterweather/Common/Common.java
@@ -0,0 +1,5 @@
+package com.sbo.betterweather.Common;
+
+public class Common {
+    private static final String APP_ID = "59380072eaeda154ef10c298240742f1";
+}
---
+++ app/src/main/java/com/sbo/betterweather/Retrofit/RetrofitClient.java
@@ -0,0 +1,4 @@
+package com.sbo.betterweather.Retrofit;
+
+public class RetrofitClient {
+}
